ed616daa14ab1e710a20cb57e413247b2672d069
Davor Kapsa <davorkapsa@gmail.com>
2015-02-11 20:49:39 +0100

    Fix #1: Add OIB check

---
+++ valida.go
@@ -0,0 +1,40 @@
+// Package valida implements OIB and JMBG validation.
+package valida
+
+import "strconv"
+
+// OIB validate OIB number.
+func OIB(in string) bool {
+	if len(in) != 11 {
+		return false
+	}
+	// get first 10 chars
+	digits := in[:len(in)-1]
+	//
+	o := 10
+	for _, r := range digits {
+		// exit if char not digit
+		d, err := strconv.Atoi(string(r))
+		if err != nil {
+			return false
+		}
+		o += d
+		o = o % 10
+		if o == 0 {
+			o = 10
+		}
+		o *= 2
+		o = o % 11
+	}
+	// exit if last char not digit
+	last, err := strconv.Atoi(string(in[len(in)-1:]))
+	if err != nil {
+		return false
+	}
+	// calc control char
+	ctrl := 11 - o
+	if ctrl == 10 {
+		ctrl = 0
+	}
+	return ctrl == last
+}
---
+++ valida_test.go
@@ -0,0 +1,29 @@
+package valida
+
+import "testing"
+
+func TestOIB(t *testing.T) {
+	var tests = []struct {
+		in   string
+		want bool
+	}{
+		// valid
+		{"69435151530", true},
+		// too short
+		{"123", false},
+		// invalid
+		{"12345678901", false},
+		// not number
+		{"123a5b7c901", false},
+		// last invalid
+		{"1234567890a", false},
+		// empty
+		{"", false},
+	}
+	for _, tt := range tests {
+		if got := OIB(tt.in); got != tt.want {
+			t.Errorf("OIB(\"%v\") = %v; want %v",
+				tt.in, got, tt.want)
+		}
+	}
+}
