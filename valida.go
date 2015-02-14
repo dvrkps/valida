d5495ac69be021e2afd4f34878d957cec93e5cf4
Davor Kapsa <davorkapsa@gmail.com>
2015-02-14 21:54:22 +0100

    Fix #7: Add Municipal ID check

--- valida.go
+++ valida.go
@@ -43,6 +43,42 @@ func JMBG(in string) bool {
 	return false
 }
 
+// MID validate Municipal ID number.
+func MID(in string) bool {
+	if len(in) != 4 || in == "0000" {
+		return false
+	}
+	// get first 3 chars
+	digits := in[:len(in)-1]
+	coef := 4
+	zzz := 0
+	for _, r := range digits {
+		// exit if char not digit
+		d, err := strconv.Atoi(string(r))
+		if err != nil {
+			return false
+		}
+		zzz += d * coef
+		if coef == 2 {
+			coef = 8
+		}
+		coef--
+	}
+	// exit if last char not digit
+	last, err := strconv.Atoi(string(in[len(in)-1:]))
+	if err != nil {
+		return false
+	}
+	ost := zzz % 11
+	raz := 11 - ost
+	ok1 := ost == 1 && last == 0
+	ok2 := ost > 1 && ost < 11 && last == raz
+	if ok1 || ok2 {
+		return true
+	}
+	return false
+}
+
 // OIB validate OIB number.
 func OIB(in string) bool {
 	if len(in) != 11 {
--- valida_test.go
+++ valida_test.go
@@ -25,6 +25,35 @@ func ExampleOIB() {
 	// false
 }
 
+func TestMID(t *testing.T) {
+	var tests = []struct {
+		in   string
+		want bool
+	}{
+		// valid
+		{"1333", true},
+		{"4880", true},
+		// too short
+		{"123", false},
+		// invalid
+		{"1234", false},
+		// not number
+		{"1a23", false},
+		// last invalid
+		{"012a", false},
+		// all zeros
+		{"0000", false},
+		// empty
+		{"", false},
+	}
+	for _, tt := range tests {
+		if got := MID(tt.in); got != tt.want {
+			t.Errorf("MID(\"%v\") = %v; want %v",
+				tt.in, got, tt.want)
+		}
+	}
+}
+
 func TestJMBG(t *testing.T) {
 	var tests = []struct {
 		in   string
