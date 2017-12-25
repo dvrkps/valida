16a3133abcc456b9a80206d0742cb967473bd90a
Davor Kapsa <davor.kapsa@gmail.com>
2017-12-25 21:49:48 +0100

    add benchmarks

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package valida
 
 func digits(n int) []int {
-	var all []int
+	raw := make([]int, 0, 20)
+	var d int
 	for n > 0 {
-		d := n % 10
-		all = append([]int{d}, all...)
+		d = n % 10
+		raw = append(raw, d)
 		n /= 10
+	}
+
+	lenRaw := len(raw)
+	all := make([]int, 0, lenRaw)
+	for i := lenRaw - 1; i >= 0; i-- {
+		all = append(all, raw[i])
+	}
+
+	return all
+}
+
+func digitsString(in string) []int {
+	var all []int
+	for _, r := range in {
+		d := digit(string(r))
+		all = append(all, d)
 	}
 	return all
 }
--- main_test.go
+++ main_test.go
@@ -21,3 +21,39 @@ func TestDigits(t *testing.T) {
 		}
 	}
 }
+
+func TestDigitsString(t *testing.T) {
+	tests := []struct {
+		in   string
+		want []int
+	}{
+		{in: "12345", want: []int{1, 2, 3, 4, 5}},
+		{in: "", want: nil},
+	}
+	for _, tt := range tests {
+		got := digitsString(tt.in)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("digits(%v) = %v; want %v", tt.in, got, tt.want)
+		}
+	}
+}
+
+var result []int
+
+func BenchmarkDigits(b *testing.B) {
+	var r []int
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		r = digits(12345678901234)
+	}
+	result = r
+}
+
+func BenchmarkDigitsString(b *testing.B) {
+	var r []int
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		r = digitsString("12345678901234")
+	}
+	result = r
+}
