7b9eb851623a543be7e004bb47774f8b607bad61
Davor Kapsa <davor.kapsa@gmail.com>
2017-12-27 17:45:25 +0100

    clean mbs

--- mbs.go
+++ mbs.go
@@ -2,38 +2,22 @@ package valida
 
 // MBS validate MBS number.
 func MBS(in string) bool {
-	if (len(in) != 8 && len(in) != 12) || !isValidOld(in) {
+	if len(in) == 12 {
+		in = in[0:8]
+	}
+	first7, last, ok := digits(in, 8)
+	if !ok {
 		return false
 	}
 
-	ost, raz := mbsCalc(in)
-
-	return mbsValid(in, ost, raz)
-
-}
-
-func mbsCalc(in string) (int, int) {
-	digits := in[:7]
 	zzz := 0
-	for i, r := range digits {
+	for i, d := range first7 {
 		coef := 8 - i
-		// exit if char not digit
-		d := digit(string(r))
 		zzz += d * coef
 	}
 
-	ost := zzz % 11
-	raz := 11 - ost
+	rem := zzz % 11
+	diff := 11 - rem
 
-	return ost, raz
-}
-
-func mbsValid(in string, ost, raz int) bool {
-	control := digit(in[7:8])
-	ok1 := ost == 1 && control == 0
-	ok2 := ost > 1 && ost < 11 && control == raz
-	if ok1 || ok2 {
-		return true
-	}
-	return false
+	return validate(rem, 1, diff, last)
 }
