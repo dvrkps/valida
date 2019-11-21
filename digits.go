87b4a91afa9b6eaf360be2c2a29a13fe24cf94cc
Davor Kapsa <davor.kapsa@gmail.com>
2019-11-21 18:35:50 +0100

    add digits file

---
+++ digits.go
@@ -0,0 +1,44 @@
+package valida
+
+import "strconv"
+
+func digits(in string, noChars int) ([]int, int, bool) {
+	empty := []int{}
+
+	lenIn := len(in)
+	if lenIn != noChars {
+		return empty, 0, false
+	}
+
+	all := make([]int, 0, lenIn)
+
+	for _, char := range in {
+		d, err := strconv.Atoi(string(char))
+		if err != nil {
+			return empty, 0, false
+		}
+
+		all = append(all, d)
+	}
+
+	if isZero(all) {
+		return empty, 0, false
+	}
+
+	digits := all[:len(all)-1]
+	last := all[len(all)-1]
+
+	return digits, last, true
+}
+
+func isZero(all []int) bool {
+	n := 0
+
+	for _, d := range all {
+		if d == 0 {
+			n++
+		}
+	}
+
+	return n == len(all)
+}
