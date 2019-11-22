5aba900e395847f9a89fbacb337563c05eef49f2
Davor Kapsa <davor.kapsa@gmail.com>
2019-11-22 16:28:10 +0100

    clean tests

--- validate_test.go
+++ validate_test.go
@@ -3,19 +3,27 @@ package valida
 import "testing"
 
 func TestValidate(t *testing.T) {
+	const (
+		zzz11    = 11
+		zzz12    = 12
+		zzz13    = 13
+		wantRem1 = 1
+		last9    = 9
+	)
+
 	tests := []struct {
 		ok      bool
 		zzz     int
 		last    int
 		wantRem int
 	}{
-		{ok: true, zzz: 11, last: 0, wantRem: 0},
-		{ok: true, zzz: 12, last: 0, wantRem: 1},
-		{ok: true, zzz: 13, last: 9, wantRem: 0},
+		{ok: true, zzz: zzz11},
+		{ok: true, zzz: zzz12, wantRem: wantRem1},
+		{ok: true, zzz: zzz13, last: last9},
 		{},
-		{zzz: 12},
-		{zzz: 14},
+		{zzz: zzz12},
 	}
+
 	for _, tt := range tests {
 		ok := validate(tt.zzz, tt.last, tt.wantRem)
 		if ok != tt.ok {
