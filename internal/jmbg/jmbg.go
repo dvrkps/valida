57040d3d87188c3d703cdacd9de2aab00b883814
Davor Kapsa <davor.kapsa@gmail.com>
2020-05-22 13:30:42 +0200

    internal/jmbg: refactor OK

--- internal/jmbg/jmbg.go
+++ internal/jmbg/jmbg.go
@@ -5,32 +5,36 @@ import (
 	"github.com/dvrkps/valida/internal/digits"
 )
 
+// OK validate JMBG number.
 func OK(in string) bool {
-	digs, ok := digits.ParseDigits([]byte(in))
+	const noDigits = 13
+
+	d, ok := digits.New(in, noDigits)
 	if !ok {
 		return false
 	}
 
 	const (
-		noDigits = 12
-		coef2    = 2
-		coef7    = 7
-		coef8    = 8
+		coef2 = 2
+		coef7 = 7
+		coef8 = 8
 	)
 
 	coef := coef7
 
 	zzz := 0
-	for _, d := range digs.First(noDigits) {
-		zzz += d * coef
+
+	d.Range(func() {
+		zzz += d.Current() * coef
 
 		if coef == coef2 {
 			coef = coef8
 		}
+
 		coef--
-	}
+	})
 
 	const wantRem = 0
 
-	return check.OK(zzz, digs.Last(), wantRem)
+	return check.OK(zzz, d.Control, wantRem)
 }
