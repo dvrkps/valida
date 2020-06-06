6679e5e7e46cddb16ff2fb1932f4eb04bc41be91
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-06 16:21:07 +0200

    internal/mid: use testutil

--- internal/mid/mid_test.go
+++ internal/mid/mid_test.go
@@ -2,32 +2,19 @@ package mid
 
 import (
 	"testing"
+
+	"github.com/dvrkps/valida/internal/testutil"
 )
 
 func TestOK(t *testing.T) {
-	var tests = []struct {
-		in   string
-		want bool
-	}{
-		// valid
-		{"1333", true},
-		{"4880", true},
-		// too short
-		{"123", false},
-		// invalid
-		{"1234", false},
-		// not number
-		{"1a23", false},
-		// all zeros
-		{"0000", false},
-		// empty
-		{"", false},
+	var tests = []testutil.TestCase{
+		{Name: "valid", Input: "1333", Want: true},
+		{Name: "too short", Input: "123"},
+		{Name: "invalid", Input: "1234"},
+		{Name: "not a number", Input: "1a23"},
+		{Name: "zeros", Input: "0000"},
+		{Name: "empty"},
 	}
 
-	for _, tt := range tests {
-		if got := OK(tt.in); got != tt.want {
-			t.Errorf("OK(%q) = %v; want %v",
-				tt.in, got, tt.want)
-		}
-	}
+	testutil.Run(t, OK, tests)
 }
