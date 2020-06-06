e83131382a317dfa7cd6f40395a494031d449ea3
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-06 17:01:58 +0200

    internal/jmbg: rm zeros test case

--- internal/jmbg/jmbg_test.go
+++ internal/jmbg/jmbg_test.go
@@ -8,13 +8,11 @@ import (
 
 func TestOK(t *testing.T) {
 	var tests = []testutil.TestCase{
-		{Name: "valid 1", Input: "0308964384007", Want: true},
-		{Name: "valid 2", Input: "0123456788100", Want: true},
+		{Name: "valid", Input: "0308964384007", Want: true},
 		{Name: "too short", Input: "123"},
 		{Name: "invalid", Input: "1234567890123"},
 		{Name: "not a number", Input: "123a567b90123"},
-		{Name: "zeros", Input: "0000000000000"},
-		{Name: "empty", Input: ""},
+		{Name: "empty"},
 	}
 
 	testutil.Run(t, OK, tests)
