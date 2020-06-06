947642d0cd95c0a112fb874892f59df25e5b9cc7
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-06 20:28:26 +0200

    internal/testutil: use t.Parallel

--- internal/testutil/main.go
+++ internal/testutil/main.go
@@ -11,9 +11,11 @@ type TestCase struct {
 
 // Run runs tests.
 func Run(t *testing.T, fn func(string) bool, testCases []TestCase) {
+	t.Parallel()
 	for _, tt := range testCases {
 		tt := tt
 		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
 			got := fn(tt.Input)
 			if got != tt.Want {
 				t.Fatalf("got %v; want %v", got, tt.Want)
