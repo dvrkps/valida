b407e1a32fb129d16ac2357f8c7d592d93207b5f
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-06 15:50:11 +0200

    internal/testutil: add Run and TestCase

--- internal/testutil/main.go
+++ internal/testutil/main.go
@@ -1,1 +1,25 @@
 package testutil
+
+import "testing"
+
+// FnOK is validate function.
+type FnOK func(string) bool
+
+// TestCase is test case data.
+type TestCase struct {
+	Name  string
+	Input string
+	Want  bool
+}
+
+func Run(t *testing.T, fn FnOK, testCases []TestCase) {
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.Name, func(t *testing.T) {
+			got := fn(tt.Input)
+			if got != tt.Want {
+				t.Fatalf("got %v; want %v", got, tt.Want)
+			}
+		})
+	}
+}
--- internal/testutil/main_test.go
+++ internal/testutil/main_test.go
@@ -1,1 +1,20 @@
 package testutil
+
+import "testing"
+
+func TestRun(t *testing.T) {
+	tests := []TestCase{
+		{Name: "ok",
+			Input: "valid",
+			Want:  true,
+		},
+		{Name: "not-ok",
+			Input: "invalid",
+			Want:  false,
+		},
+	}
+
+	testFn := func(in string) bool { return in == "valid" }
+
+	Run(t, testFn, tests)
+}
