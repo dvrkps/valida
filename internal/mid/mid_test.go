a1c82a33da934035877a142b8df576db351ab852
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-06 17:04:41 +0200

    internal/mid: clean testcases

--- internal/mid/mid_test.go
+++ internal/mid/mid_test.go
@@ -12,7 +12,6 @@ func TestOK(t *testing.T) {
 		{Name: "too short", Input: "123"},
 		{Name: "invalid", Input: "1234"},
 		{Name: "not a number", Input: "1a23"},
-		{Name: "zeros", Input: "0000"},
 		{Name: "empty"},
 	}
 
